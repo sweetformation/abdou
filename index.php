c9db816773d6e73da25799a6ed4ae79540292a9a
guillaume <gsylvestre@gmail.com>
2014-10-08 20:57:15 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,237 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Guide de parapente au Maroc | Abderahim Derbani</title>
+<meta name="Description" content="Abderrahim Derbani, guide de parapente au Maroc, sur les circuits d'Agadir et de Marrakech." />
+
+		
+	<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+	<link rel="shortcut icon" type="image/x-icon" href="img/favicon.png">
+	
+	<script type="text/javascript">
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-2427391-50']);
+	  _gaq.push(['_trackPageview']);
+	  (function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+	</script>
+</head>
+
+<body>
+
+	<div id="container">
+    
+    <div id="header">
+        <div id="bijour">
+            <span class="font85">Bijour</span><span class="font29">. Je suis <strong>Abderrahim Derbani</strong></span><br/>
+            <span class="font23"> &nbsp;et je suis <strong><span class="font35">guide</span></strong> pour <strong><span class="font35">parapentistes</span></strong> au <strong><span class="font35">Maroc</span></strong></span>
+        </div>
+    </div>
+    
+    <div id="content">
+    
+        <div id="left">
+        
+             <div id="postit">
+                 <!--<p>Depuis plus de 10 ans, j'organise des <strong>séjours de parapente sur mesure au Maroc</strong>, avec <a href="http://www.lesgensdair.com/maroc.htm" title="Voyage de parapente au Maroc" target="_blank">Les Gens d'Air et Vincent Brisard</a>. Mes clients me laisse penser qu'ils sont satisfaits, du moins si j'en juge d'après le montant des pourboires ; )</p>
+                <p>En transformant votre séjour au Maroc en un pur plaisir (inch'allah), mon objectif premier est de vous voir revenir parapenter ici, année après année...</p>                
+                <p>Amis parapentistes : bienvenue chez nous.</p>-->
+                
+                
+                <p>A juste 2h d'avion d'Europe, <b>le Maroc</b> est un paradis pour parapentistes, une invitation au voyage et aux rencontres.</p>
+
+				<p>Je vous propose de (re?)découvrir mon pays en pratiquant <b>le parapente</b>. Je suis moi même pilote, et, depuis plus de dix ans, j'organise des séjours et j'accompagne des groupes de parapentistes à travers le Maroc, avec <a href="http://www.lesgensdair.com/maroc.htm" title="Voyage de parapente au Maroc" target="_blank">Les Gens d'Air et Vincent Brisard</a>.</p>
+
+               <p>Le Maroc est un pays idéal pour le parapentiste à la recherche de dépaysement et d'authenticité.</p>
+                
+
+            </div>
+            
+            
+            
+             <div id="contact">
+                <h2>Contact</h2>
+                <h3>Abderrahim Derbani</h3>
+                    Tel : +212 (0)6 64 38 94 27<br /> 
+                    Fax : +212 (0)6 22 99 41 21<br />
+                    Email : <a href="mailto:derbaniwaga@hotmail.com" target="_blank">derbaniwaga@hotmail.com</a>
+                    
+                     <p><b>Bon à savoir :</b> Mes tarifs sont en fonction du nombre de participants ainsi que des prestations souhaitées. Je suis flexible et à votre disposition pour organiser des séjours sur mesure.</p>
+                     
+                     <p>Si vous avez des projets de vol au Maroc ou simplement des questions, n'hésitez pas à <a href="mailto:derbaniwaga@hotmail.com" target="_blank">me contacter</a>.</p>
+                    
+                    <!--<p><b>Bon à savoir :</b> plus vous êtes nombreux à vous inscrire, plus mes prix sont interressants!<br /><br />
+                    Pour connaître mes tarifs, <a href="mailto:derbaniwaga@hotmail.com" target="_blank">contactez-moi</a>.</p>-->
+            </div>
+        
+        </div>
+        
+        <div id="right">
+        
+            <div id="services">
+                <h2>Services</h2>
+                Je me charge de l'intendance de votre séjour : transport, hôtels et gites, repas. Je vous servirai de guide et d'interprète
+                et je vous trouverai les meilleurs endroits pour que vous puissiez assouvir votre passion.<br />
+                <span class="tagline">Vous ne faites que gonfler, voler, manger, rire, dormir. Le reste, je m'en occupe...</span><br /><br />
+                <table><tr>
+                    <td class="transport"></td>
+                    <td class="tddesc">
+                        <h3>Transport</h3>
+                        Transfert de et vers l'aéroport, navettes entre l'attéro et le déco, courses locales avant le départ...</td>
+                    <td class="hebergement"></td>
+                    <td class="tddesc">
+                        <h3>Hébergement</h3>
+                        Réservations d'hôtels et d'auberges confortables, directement sur le déco lorsque possible.
+                    </td>
+                </tr><tr>
+                    <td class="repas"></td>
+                    <td class="tddesc">
+                        <h3>Repas</h3>
+                        Réservation dans des restaurants typiques, préparation des déjeuners directement sur le site, au besoin.
+                    </td>
+                    <td class="guide"></td>
+                    <td class="tddesc">
+                        <h3>Guide</h3>
+                        Proposition de circuits en fonction de l'aérologie, visites locales, renseignements culturels.
+                    </td>
+                </tr></table>
+            </div>
+            
+            <br />
+            
+            <div id="services">
+                <h2>Circuits</h2>
+                
+                <!--Je m'adapte à chaque groupe et mes circuits sont flexibles. Cependant, ces 2 circuits sont plus souvent pratiqués, car ils offrent une grande variété de sites intéressants.<br><br>-->
+                
+               2 circuits sont plus souvent pratiqués, car ils offrent une grande variété de sites intéressants. Cependant, je suis à votre disposition pour vous proposer d'autres destinations selon vos souhaits. Et, en cours de séjour, si la météo nous y oblige, nous pourrons bien sur modifier le parcours pour aller là où ça vole !<br /><br />
+                <table><tr>
+                    <td class="agadir"></td>
+                    <td class="tddesc">
+                        <h3>Agadir</h3>
+                        Transferts aéroport d'Agadir, vols littoral principalement, auberges pieds-dans-l'eau, jusqu'à 300m de dénivelé...
+                    <td class="marrakech"></td>
+                    <td class="tddesc">
+                        <h3>Marrakech</h3>
+                        Transferts aéroport de Marrakech, vols en montagne, vue sur les sommets de l'Atlas, longs vols thermiques...
+                    </td>
+                </tr></table>
+                
+                <br />
+                <ul>
+                    <li>Disponible de septembre à janvier (de février à mai, contactez <a href="http://www.lesgensdair.com/maroc.htm" title="Voyage de parapente au Maroc" target="_blank">Les Gens d'Air</a>).</li>
+                    <li>Pour voler avec moi, vous devez être un pilote breveté.</li>
+                    <li>Avant le départ, vous devez impérativement avoir rempli les conditions d'autorisation de vol au Maroc.</li>
+                </ul>
+            </div>
+        
+        </div>
+    
+       
+    </div>
+    
+    <div class="clear"></div>
+    
+    <br /><br />
+    
+    <div id="footerborder"></div>
+    
+    <div id="footer">
+    
+    <a rel="shadowbox" href="img/pic/abdou-parapente-maroc-5.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-5.jpg" class="pics"></a>
+    <a rel="shadowbox" href="img/pic/abdou-parapente-maroc-23.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-23.jpg" class="pics"></a>
+    <a rel="shadowbox" href="img/pic/abdou-parapente-maroc-3.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-3.jpg" class="pics"></a>
+    <a rel="shadowbox" href="img/pic/abdou-parapente-maroc-22.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-22.jpg" class="pics"></a>
+    <a rel="shadowbox" href="img/pic/abdou-parapente-maroc-6.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-6.jpg" class="pics"></a>
+    <a rel="shadowbox" href="img/pic/abdou-parapente-maroc-36.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-36.jpg" class="pics"></a>
+    <a rel="shadowbox" href="img/pic/abdou-parapente-maroc-10.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-10.jpg" class="pics"></a>
+    <a rel="shadowbox" href="img/pic/abdou-parapente-maroc-11.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-11.jpg" class="pics"></a>
+    <a rel="shadowbox" href="img/pic/abdou-parapente-maroc-12.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-12.jpg" class="pics"></a>
+    <a rel="shadowbox" href="img/pic/abdou-parapente-maroc-24.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-24.jpg" class="pics"></a>
+    <a rel="shadowbox" href="img/pic/abdou-parapente-maroc-26.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-26.jpg" class="pics"></a>
+    <a rel="shadowbox" href="img/pic/abdou-parapente-maroc-30.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-30.jpg" class="pics"></a>
+    <a rel="shadowbox" href="img/pic/abdou-parapente-maroc-41.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-41.jpg" class="pics"></a>
+    <a rel="shadowbox" href="img/pic/abdou-parapente-maroc-31.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-31.jpg" class="pics"></a>
+    <a rel="shadowbox" href="img/pic/abdou-parapente-maroc-32.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-32.jpg" class="pics"></a>
+    <a rel="shadowbox" href="img/pic/abdou-parapente-maroc-43.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-43.jpg" class="pics"></a>
+    <a rel="shadowbox" href="img/pic/abdou-parapente-maroc-1.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-1.jpg" class="pics"></a>
+    <a rel="shadowbox" href="img/pic/abdou-parapente-maroc-46.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-46.jpg" class="pics"></a>
+    
+    <!--
+    <a rel="shadowbox[abdou]" href="img/pic/abdou-parapente-maroc-2.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-2.jpg" class="pics"></a>
+    <a rel="shadowbox[abdou]" href="img/pic/abdou-parapente-maroc-4.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-4.jpg" class="pics"></a>
+    <a rel="shadowbox[abdou]" href="img/pic/abdou-parapente-maroc-7.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-7.jpg" class="pics"></a>
+    <a rel="shadowbox[abdou]" href="img/pic/abdou-parapente-maroc-8.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-8.jpg" class="pics"></a>
+    <a rel="shadowbox[abdou]" href="img/pic/abdou-parapente-maroc-9.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-9.jpg" class="pics"></a>
+    <a rel="shadowbox[abdou]" href="img/pic/abdou-parapente-maroc-13.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-13.jpg" class="pics"></a>
+    <a rel="shadowbox[abdou]" href="img/pic/abdou-parapente-maroc-14.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-14.jpg" class="pics"></a>
+    <a rel="shadowbox[abdou]" href="img/pic/abdou-parapente-maroc-15.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-15.jpg" class="pics"></a>
+    <a rel="shadowbox[abdou]" href="img/pic/abdou-parapente-maroc-16.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-16.jpg" class="pics"></a>
+    <a rel="shadowbox[abdou]" href="img/pic/abdou-parapente-maroc-17.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-17.jpg" class="pics"></a>
+    <a rel="shadowbox[abdou]" href="img/pic/abdou-parapente-maroc-18.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-18.jpg" class="pics"></a>
+    <a rel="shadowbox[abdou]" href="img/pic/abdou-parapente-maroc-19.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-19.jpg" class="pics"></a>
+    <a rel="shadowbox[abdou]" href="img/pic/abdou-parapente-maroc-20.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-20.jpg" class="pics"></a>
+    <a rel="shadowbox[abdou]" href="img/pic/abdou-parapente-maroc-21.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-21.jpg" class="pics"></a>
+    <a rel="shadowbox[abdou]" href="img/pic/abdou-parapente-maroc-25.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-25.jpg" class="pics"></a>
+    <a rel="shadowbox[abdou]" href="img/pic/abdou-parapente-maroc-27.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-27.jpg" class="pics"></a>
+    <a rel="shadowbox[abdou]" href="img/pic/abdou-parapente-maroc-28.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-28.jpg" class="pics"></a>
+    <a rel="shadowbox[abdou]" href="img/pic/abdou-parapente-maroc-29.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-29.jpg" class="pics"></a>
+    <a rel="shadowbox[abdou]" href="img/pic/abdou-parapente-maroc-33.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-33.jpg" class="pics"></a>
+    <a rel="shadowbox[abdou]" href="img/pic/abdou-parapente-maroc-34.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-34.jpg" class="pics"></a>
+    <a rel="shadowbox[abdou]" href="img/pic/abdou-parapente-maroc-35.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-35.jpg" class="pics"></a>
+    <a rel="shadowbox[abdou]" href="img/pic/abdou-parapente-maroc-37.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-37.jpg" class="pics"></a>
+    <a rel="shadowbox[abdou]" href="img/pic/abdou-parapente-maroc-38.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-38.jpg" class="pics"></a>
+    <a rel="shadowbox[abdou]" href="img/pic/abdou-parapente-maroc-39.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-39.jpg" class="pics"></a>
+    <a rel="shadowbox[abdou]" href="img/pic/abdou-parapente-maroc-40.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-40.jpg" class="pics"></a>
+    <a rel="shadowbox[abdou]" href="img/pic/abdou-parapente-maroc-42.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-42.jpg" class="pics"></a>
+    <a rel="shadowbox[abdou]" href="img/pic/abdou-parapente-maroc-44.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-44.jpg" class="pics"></a>
+    <a rel="shadowbox[abdou]" href="img/pic/abdou-parapente-maroc-45.jpg"><img src="img/pic/thumbs/abdou-parapente-maroc-45.jpg" class="pics"></a>-->
+        
+    <div class="clear"></div><br />
+    
+    <div id="subfooter">&copy;Abderrahim Derbani 2012 | site par <a href="http://www.webolic.com" target="_blank">Webolic</a></div>
+    
+    </div>
+    
+</div>
+
+
+<script type="text/javascript"  src="jquery-1.4.3.min.js"></script>
+    
+<link rel="stylesheet" type="text/css" href="fancybox/jquery.fancybox-1.3.4.css" media="screen" />
+<script type="text/javascript" src="fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
+<script type="text/javascript" src="fancybox/jquery.fancybox-1.3.4.pack.js"></script>
+<script type="text/javascript">
+$("a[rel=shadowbox]").fancybox({
+				'cyclic' : 	true,
+				//'autoScale' : 	true,
+				'centerOnScroll ' :	true,
+				'overlayOpacity' : 	0.8,
+				'overlayColor' : '#000',
+				'titleShow' : 	false,
+				'transitionIn'	: 'fade',
+				'transitionOut'	: 'fade'
+			});
+</script>
+
+	
+    
+<!--<link rel="stylesheet" type="text/css" href="shadowbox-3.0.3/shadowbox.css">
+<script type="text/javascript" src="shadowbox-3.0.3/shadowbox.js"></script>
+<script type="text/javascript">
+Shadowbox.init({
+    handleOversize: "drag",
+	continuous : true,
+	counterType : "skip",
+	counterLimit : 18,
+	overlayOpacity : 0.8
+});
+</script>-->
+
+		
+</body>
+</html>
