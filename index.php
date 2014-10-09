65c9ff909881432460927cc434ee9652ccb234b9
Calimero <florianseailles@gmail.com>
2014-10-09 15:47:04 +0200

    table remplacé

--- index.php
+++ index.php
@@ -64,61 +64,74 @@
         
         <div id="right">
         
-            <div id="services">
+            <div>
                 <h2>Services</h2>
                 Je me charge de l'intendance de votre séjour : transport, hôtels et gites, repas. Je vous servirai de guide et d'interprète
                 et je vous trouverai les meilleurs endroits pour que vous puissiez assouvir votre passion.<br />
                 <span class="tagline">Vous ne faites que gonfler, voler, manger, rire, dormir. Le reste, je m'en occupe...</span><br /><br />
-                <table><tr>
-                    <td class="transport"></td>
-                    <td class="tddesc">
-                        <h3>Transport</h3>
-                        Transfert de et vers l'aéroport, navettes entre l'attéro et le déco, courses locales avant le départ...</td>
-                    <td class="hebergement"></td>
-                    <td class="tddesc">
-                        <h3>Hébergement</h3>
-                        Réservations d'hôtels et d'auberges confortables, directement sur le déco lorsque possible.
-                    </td>
-                </tr><tr>
-                    <td class="repas"></td>
-                    <td class="tddesc">
-                        <h3>Repas</h3>
-                        Réservation dans des restaurants typiques, préparation des déjeuners directement sur le site, au besoin.
-                    </td>
-                    <td class="guide"></td>
-                    <td class="tddesc">
-                        <h3>Guide</h3>
-                        Proposition de circuits en fonction de l'aérologie, visites locales, renseignements culturels.
-                    </td>
-                </tr></table>
+                <div class="servicesimg">
+                    <div class="transport1">
+                        <div class="transport"></div>
+                        <div class="tddesc">
+                            <h3>Transport</h3>
+                            Transfert de et vers l'aéroport, navettes entre l'attéro et le déco, courses locales avant le départ...
+                        </div>
+                    </div>
+                    <div class="hebergement1">
+                        <div class="hebergement"></div>
+                        <div class="tddesc">
+                            <h3>Hébergement</h3>
+                            Réservations d'hôtels et d'auberges confortables, directement sur le déco lorsque possible.
+                        </div>
+                    </div>
+                    <div class="repas1">
+                        <div class="repas"></div>
+                        <div class="tddesc">
+                            <h3>Repas</h3>
+                            Réservation dans des restaurants typiques, préparation des déjeuners directement sur le site, au besoin.
+                        </div>
+                    </div>
+                    <div class="guide1">
+                        <div class="guide"></div>
+                        <div class="tddesc">
+                            <h3>Guide</h3>
+                            Proposition de circuits en fonction de l'aérologie, visites locales, renseignements culturels.
+                        </div>
+                    </div>
+                </div>
             </div>
-            
+               
             <br />
             
-            <div id="services">
+            <div>
                 <h2>Circuits</h2>
                 
                 <!--Je m'adapte à chaque groupe et mes circuits sont flexibles. Cependant, ces 2 circuits sont plus souvent pratiqués, car ils offrent une grande variété de sites intéressants.<br><br>-->
                 
                2 circuits sont plus souvent pratiqués, car ils offrent une grande variété de sites intéressants. Cependant, je suis à votre disposition pour vous proposer d'autres destinations selon vos souhaits. Et, en cours de séjour, si la météo nous y oblige, nous pourrons bien sur modifier le parcours pour aller là où ça vole !<br /><br />
-                <table><tr>
-                    <td class="agadir"></td>
-                    <td class="tddesc">
-                        <h3>Agadir</h3>
-                        Transferts aéroport d'Agadir, vols littoral principalement, auberges pieds-dans-l'eau, jusqu'à 300m de dénivelé...
-                    <td class="marrakech"></td>
-                    <td class="tddesc">
-                        <h3>Marrakech</h3>
-                        Transferts aéroport de Marrakech, vols en montagne, vue sur les sommets de l'Atlas, longs vols thermiques...
-                    </td>
-                </tr></table>
+                <div class="circuits">
+                    <div class="agadir1">
+                        <div class="agadir"></div>
+                        <div class="tddesc">
+                            <h3>Agadir</h3>
+                            Transferts aéroport d'Agadir, vols littoral principalement, auberges pieds-dans-l'eau, jusqu'à 300m de dénivelé...
+                        </div>
+                    </div>
+                    <div class="marrakech1">
+                        <div class="marrakech"></div>
+                        <div class="tddesc">
+                            <h3>Marrakech</h3>
+                            Transferts aéroport de Marrakech, vols en montagne, vue sur les sommets de l'Atlas, longs vols thermiques...
+                        </div>
+                    </div>
+                </div>
                 
                 <br />
-                <ul>
-                    <li>Disponible de septembre à janvier (de février à mai, contactez <a href="http://www.lesgensdair.com/maroc.htm" title="Voyage de parapente au Maroc" target="_blank">Les Gens d'Air</a>).</li>
-                    <li>Pour voler avec moi, vous devez être un pilote breveté.</li>
-                    <li>Avant le départ, vous devez impérativement avoir rempli les conditions d'autorisation de vol au Maroc.</li>
-                </ul>
+                <div class="p">
+                    <p class="tik">Disponible de septembre à janvier (de février à mai, contactez <a href="http://www.lesgensdair.com/maroc.htm" title="Voyage de parapente au Maroc" target="_blank">Les Gens d'Air</a>).</p>
+                    <p class="tik">Pour voler avec moi, vous devez être un pilote breveté.</p>
+                    <p class="tik">Avant le départ, vous devez impérativement avoir remplir les conditions d'autorisation de vol au Maroc.</p>
+                </div>
             </div>
         
         </div>
